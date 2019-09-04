235f05453b6d249b7923296dad7e41f8590975f3
James Smit <jamessmit1222@gmail.com>
2019-09-03 20:48:53 -0700

    adding keys and updating to make google accounts work

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '369700097699-gtcro9kj2lj5fna4p7i6jqrs6u9p1mok.apps.googleusercontent.com',
+        clientSecret: 'dLyNe1f35nvskaY7IdGKhn4e'
+    }
+};
