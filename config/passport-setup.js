209db2724ae0b237a2e59db540eb3f5ad1811e4d
James Smit <jamessmit1222@gmail.com>
2019-09-05 18:27:50 -0700

    google login working

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,4 +1,3 @@
-
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
@@ -9,7 +8,9 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
-    }, () => {
+    }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
+        console.log('passport callback function fired:');
+        console.log(profile);
     })
 );
